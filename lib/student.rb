b0585ae62ae4f0b30e1c47522b9d0f41523841e4
Mariam Haider <mfh.haider@gmail.com>
2020-04-22 22:11:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,16 +49,10 @@ class Student
   end 
 
   def self.find_by_name(name)
-     sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
+    sql = "SELECT * FROM students WHERE name = ?"
 
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
+    result = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(result)
   end
 
   def update

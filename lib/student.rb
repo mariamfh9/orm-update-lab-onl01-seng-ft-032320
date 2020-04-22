4a6672159bc8ea06e826f917f24e7f0bea5b5bbd
Mariam Haider <mfh.haider@gmail.com>
2020-04-22 22:16:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,10 @@ class Student
   end
 
   def self.new_from_db(row)
-    student_new = self.new(row[0], row[1], row[2])
-    student_new
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end 
 
   def self.find_by_name(name)
